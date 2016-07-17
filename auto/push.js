7bc5b5861fc45e7684205ef53e44e9f36d717faf
UKkkkkkkkkk <825846102@qq.com>
2016-07-17 11:45:01 +0800

    提交信息

--- auto/push.js
+++ auto/push.js
@@ -1,7 +1,7 @@
 var CronJob = require('cron').CronJob;
 var exec = require('child_process').exec;
 var str = '提交信息';
- var job = new CronJob('50 28 * * * *',function(){
+ var job = new CronJob('0 * * * * *',function(){
  exec('git add -A',function(err,stdout,stderr){
  exec('git commit -m"'+str+'"',function(err,stdout,stderr){
  exec('git push origin master',function(err,stdout,stderr){
